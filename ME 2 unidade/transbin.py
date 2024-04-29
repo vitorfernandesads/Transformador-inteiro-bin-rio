c15173b1a90f6d54eec8752c60046b3a35fb2b9d
vitorfernandesads <168485808+vitorfernandesads@users.noreply.github.com>
2024-04-29 19:16:22 -0300

    Update transbin.py

--- ME 2 unidade/transbin.py
+++ ME 2 unidade/transbin.py
@@ -1,10 +1,10 @@
 while True:
     print("Tranformando um número inteiro em binário. ")
     sleep(1)
-    n = int(input("Insira um número inteiro: "))
+    numero = int(input("Insira um número inteiro: "))
     sleep(1)
-    x = bin(n)
+    b = bin(n)
     if n == 0:
-        print("Encerrado o programa!")
+        print("Encerrado o programa")
         break
-    print(f"O número {n} convertido para binário equivale a {str(x)[2:]}")
+    print(f"O número {numero} convertido para binário equivale a {str(b)[2:]}")
